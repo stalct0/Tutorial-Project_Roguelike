361e24bcb238ab278755468a2a0e1f2eed982bfd
박채호 <iloveramen9753@gmail.com>
2025-07-28 21:58:34 +0900

    FeatureUI

---
+++ Assets/scripts/UI/CharacterSelector.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CharacterSelector : MonoBehaviour
+{
+    public void SelectRed()
+    {
+        Debug.Log("물리학과 캐릭터 생성");
+    }
+
+    public void SelectBlue()
+    {
+        Debug.Log("소프트웨어학부 캐릭터 생성");
+    }
+
+    public void SelectGreen()
+    {
+        Debug.Log("미디어 커뮤니케이션학과 캐릭터 생성");
+    }
+
+    public void SelectYellow()
+    {
+        Debug.Log("화학과 캐릭터 생성");
+    }
+}
---
+++ Assets/scripts/UI/StatDisplay.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class StatDisplay : MonoBehaviour
+{
+    public TMP_Text statText;
+
+    void Start()
+    {
+        SetStat(99);  // ✔️ 이게 맞는 함수
+    }
+
+    public void SetStat(int power)
+    {
+        statText.text = $"{power}";
+    }
+}
